024b837e399a551d471fe5628e65b9406fc65aef
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-10-18 22:38:10 +0200

    DI

--- PizzaStore.WPF/App.xaml.cs
+++ PizzaStore.WPF/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using PizzaStore.WPF.ViewModels;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PizzaStore.Domain.Interfaces;
+using PizzaStore.Domain.Models;
+using PizzaStore.Infrastructure.Services;
+using PizzaStore.WPF.State.Navigators;
+using PizzaStore.WPF.ViewModels;
+using System;
 using System.Windows;
 
 namespace PizzaStore.WPF
@@ -10,14 +16,29 @@ namespace PizzaStore.WPF
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            IServiceProvider serviceProvider = CreateServiceProvider();
+            IDataService<Product> productDataService = serviceProvider.GetRequiredService<IDataService<Product>>();
+
             Window window = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = serviceProvider.GetRequiredService<MainViewModel>()
             };
 
             window.Show();
 
             base.OnStartup(e);
+        }
+
+        private IServiceProvider CreateServiceProvider()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<IDataService<Product>, HardCodedProductDataService>();
+
+            services.AddScoped<INavigator, Navigator>();
+            services.AddScoped<MainViewModel>();
+
+            return services.BuildServiceProvider();
         }
     }
 }
