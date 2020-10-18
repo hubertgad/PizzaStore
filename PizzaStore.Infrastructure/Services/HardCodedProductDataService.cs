de11c4238bdf3c48c8e5acc61300622a8b4d08e0
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-10-18 22:38:19 +0200

    Create HardCodedProductDataService.cs

---
+++ PizzaStore.Infrastructure/Services/HardCodedProductDataService.cs
@@ -0,0 +1,19 @@
+﻿using PizzaStore.Domain.Interfaces;
+using PizzaStore.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaStore.Infrastructure.Services
+{
+    public class HardCodedProductDataService : IDataService<Product>
+    {
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1, Name = "Margaritta", Price = 20, Symbol = "MARG", Type = Type.Pizza },
+                new Product { Id = 2, Name = "Vegetariana", Price = 22, Symbol = "VEGE", Type = Type.Pizza }
+            };
+        }
+    }
+}
