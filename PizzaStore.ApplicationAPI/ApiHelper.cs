36b9d28624c4e74121bc69945fc9c85b43be1768
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-11-25 23:17:26 +0100

    Create ApiHelper.cs

---
+++ PizzaStore.ApplicationAPI/ApiHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PizzaStore.ApplicationApi
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        static ApiHelper()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
