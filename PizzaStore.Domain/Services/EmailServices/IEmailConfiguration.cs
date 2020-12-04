33c25729d30538747a0a7f4944c5d347a36c2168
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-12-04 16:10:18 +0100

    Update IEmailConfiguration.cs

--- PizzaStore.Domain/Services/EmailServices/IEmailConfiguration.cs
+++ PizzaStore.Domain/Services/EmailServices/IEmailConfiguration.cs
@@ -3,8 +3,11 @@
     public interface IEmailConfiguration
     {
         string SmtpServer { get; }
+
         int SmtpPort { get; }
+
         string SmtpUsername { get; set; }
+
         string SmtpPassword { get; set; }
     }
 }
